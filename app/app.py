f98dc196a5bf96d8d8386b521b4d7272daab44d1
darkdevil86 <iamvamshikrishna1210@gmail.com>
2025-04-10 22:35:20 +0530

    fixed docker pull

--- app/app.py
+++ app/app.py
@@ -3,10 +3,10 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 
-@app.route("/hello", methods=["GET"])
+@app.route("/", methods=["GET"])
 def hello():
-    return jsonify({"message": "Hello, World!\nLogging  to your account!"})
+    return jsonify({"message": "Hello, World! Logging to your account!"})
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
